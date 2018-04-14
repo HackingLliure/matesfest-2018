fb98ebf4dc9b2e207bc4fe2528c3495a1c0000c7
Taras Yarema <tarasyaremabcn@gmail.com>
2018-04-14 20:52:02 +0200

    Taras commit

--- crypto/controllers/home.js
+++ crypto/controllers/home.js
@@ -16,38 +16,36 @@ signature = sign.sign(privateKey);
 console.log(verify.verify(publicKey, signature));  
 */
 
-const crypto = require('crypto');
 const NodeRSA = require('node-rsa');
 let key = new NodeRSA();
+let keyHash = new NodeRSA({b: 512});
 
 exports.index = function(req, res) {
   // Get session cookies
   cookies = req.cookies;
 
-  //console.log(crypto.getCurves());
-  //console.log(crypto.getHashes());
+  // Check if session cookie exists
+  if(!cookies["id"]) {
 
-  // Cookie stuff
-  if(!cookies["public-id"]) {
+    // Low e
+    key.generateKeyPair(128, 2111);
+    id = keyHash.encrypt( key.exportKey('public') , 'hex').substring(0, 8);
+    cookies["id"] =  [ id, key.exportKey('public'), key.exportKey('private') ];
 
-    key.generateKeyPair(256, 9109);
-    id = crypto.createHash('sha256').update(key.exportKey('public')).digest("base64").substring(0, 8);
-    cookies["public-id"] =  [ id, key.exportKey('public'), key.exportKey('private') ];
-    
-    res.cookie("public-id", cookies["public-id"][0]);
-    res.cookie("public-key", cookies["public-id"][1]);
-    res.cookie("private-key", cookies["public-id"][2]);
+    res.cookie("id", cookies["id"][0]);
+    res.cookie("public-key", cookies["id"][1]);
+    res.cookie("private-key", cookies["id"][2]);
     
     res.render('_home', {
       title: 'Home',
-      id: cookies["public-id"]
+      id: cookies["id"]
     });
 
   } else {
 
     res.render('home', {
       title: 'Home',
-      id: cookies["public-id"]
+      id: cookies["id"]
     });
 
   }
