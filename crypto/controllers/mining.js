55aa410dceb94794b581586c33f27df3ee0c88d7
2pac <tarasyaremabcn@gmail.com>
2018-04-17 18:13:05 +0200

    Workin on the mining.

--- crypto/controllers/mining.js
+++ crypto/controllers/mining.js
@@ -1,5 +1,7 @@
 const NodeRSA = require('node-rsa');
 const sqlite3 = require('sqlite3');
+const async = require('async');
+
 let blockchain_db = new sqlite3.Database('blockchain.sqlite3', (err) => {
   if (err) {
 	console.error(err.message);
@@ -15,7 +17,11 @@ let private_db = new sqlite3.Database('private.sqlite3', (err) => {
 });
 
 function get_pending_transactions(callback) {
-	blockchain_db.all(`SELECT * FROM transactions WHERE signature is '';`, callback);	
+	blockchain_db.all(`SELECT * FROM transactions;`, callback);	
+}
+
+function get_public(from, callback){
+	private_db.get(`SELECT id,public,private FROM accounts WHERE id = ?;`, [from], callback);
 }
 
 exports.index = function(req, res) {
@@ -24,23 +30,42 @@ exports.index = function(req, res) {
 	get_pending_transactions(function (err, rows) {
 		if (err) {
 			console.log(err);
-		} else {
-			rows.forEach(function (row) {
-				let buffer = "";
-
-				let public = private_db.get(`SELECT * FROM accounts WHERE id = ?;`, [row.to], 
-					function (err, sol) {
-						buffer += "";
-						console.log(sol.public);
-				});
-
-				buffer += row.amount;
-				console.log(buffer);
-			});
+			return;
 		}
-	});
+		let checked_signatures = [];
 
-	res.render('mining', {
-      title: 'Mining'
-    });
+		
+		async.forEachOf(rows, function (row, key, callback) {
+			get_public(row.from,
+				function (err, sol) {
+					if (err) {
+						return callback(err);
+					} else if (sol != undefined) {
+						let buffer = row.from + row.to + row.amount;
+						let key = new NodeRSA();
+						key.importKey(sol.public);
+
+						if (key.verify(buffer, new Buffer(row.signature, 'base64'))) {
+							checked_signatures.push({signature: row.signature, correct: "True"});
+						} else {
+							checked_signatures.push({signature: row.signature, correct: "False"});
+						}
+						callback();	
+					}
+			});
+		}, function (err) {
+			if (err) {
+				console.log(err.message);
+				return;
+			}
+
+			res.render('mining', {
+		      title: 'Mining',
+		      signatures: checked_signatures
+		    });
+		});
+
+		
+
+	});
 }
